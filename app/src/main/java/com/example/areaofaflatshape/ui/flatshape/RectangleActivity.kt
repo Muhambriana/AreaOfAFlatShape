774b89f9c752dc73d175bed155dbff4686a1a06c
Muham <muhambriana.31@gmail.com>
2024-07-09 20:30:09 +0700

    update RectangleActivity

--- app/src/main/java/com/example/areaofaflatshape/ui/flatshape/RectangleActivity.kt
+++ app/src/main/java/com/example/areaofaflatshape/ui/flatshape/RectangleActivity.kt
@@ -22,5 +22,40 @@ class RectangleActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        initFunction()
+    }
+
+    private fun initFunction() {
+        // Init EditText that will be form long of rectangle
+        val edLong: EditText = findViewById(R.id.ed_long)
+        // Init EditText that will be form wide of rectangle
+        val edWide: EditText = findViewById(R.id.ed_wide)
+        // Init Button to calculate
+        val btnResult: Button = findViewById(R.id.btn_result)
+        // Init TextView for showing the result
+        val tvResult: TextView = findViewById(R.id.tv_result)
+
+        // Set listener on button click
+        btnResult.setOnClickListener {
+            // If long or wide not filled yet. Than show toast message
+            if (edLong.text.isNullOrBlank() || edWide.text.isNullOrBlank()) {
+                // Show message as toast
+                Toast.makeText(this, "Harap Isi Panjang Dan Lebar Terlebih Dahulu", Toast.LENGTH_LONG).show()
+                return@setOnClickListener // This will return value to setOnClickListener and stop the code (not execute all code below)
+            }
+
+            // Get the long value
+            val long: Double = edLong.text?.toString()?.toDouble() ?: 0.0
+            // Get the wide value
+            val wide: Double = edWide.text?.toString()?.toDouble() ?: 0.0
+            // Calculate side one and side two value with formula  (P X L)
+            val result: Double = long * wide
+
+            // Make the TextView visible
+            tvResult.visibility = View.VISIBLE
+            // Set result value to TextView
+            tvResult.text = result.toString()
+        }
     }
 }
