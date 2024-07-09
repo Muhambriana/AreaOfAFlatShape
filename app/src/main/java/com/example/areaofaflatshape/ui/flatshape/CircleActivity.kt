380734b0d1d835ff593d8252c0b1e12b903dfbb9
Muham <muhambriana.31@gmail.com>
2024-07-09 21:11:34 +0700

    update CircleActivity

--- app/src/main/java/com/example/areaofaflatshape/ui/flatshape/CircleActivity.kt
+++ app/src/main/java/com/example/areaofaflatshape/ui/flatshape/CircleActivity.kt
@@ -1,6 +1,11 @@
 package com.example.areaofaflatshape.ui.flatshape
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,6 +21,39 @@ class CircleActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        initFunction()
+    }
+
+    private fun initFunction() {
+        // Init EditText that will be form radius of Circle
+        val edRadius: EditText = findViewById(R.id.ed_radius)
+        // Init Button to calculate
+        val btnResult: Button = findViewById(R.id.btn_result)
+        // Init TextView for showing the result
+        val tvResult: TextView = findViewById(R.id.tv_result)
+
+
+        // Set listener on button click
+        btnResult.setOnClickListener {
+            // If edRadius not filled yet. Than show toast message
+            if (edRadius.text.isNullOrBlank()) {
+                // Show message as toast
+                Toast.makeText(this, "Harap Isi Jari-Jari Terlebih Dahulu", Toast.LENGTH_LONG).show()
+
+                return@setOnClickListener // This will return value to setOnClickListener and stop the code (not execute all code below)
+            }
+
+            // Get the side one value
+            val radius: Double = edRadius.text?.toString()?.toDouble() ?: 0.0
+            // Calculate radius value with formula  (3.14 * (R*R))
+            val result: Double = 3.14 * (radius * radius)
+
+            // Make the TextView visible
+            tvResult.visibility = View.VISIBLE
+            // Set result value to TextView
+            tvResult.text = result.toString()
         }
     }
 }
